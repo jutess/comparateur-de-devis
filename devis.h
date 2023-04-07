b344a1405f21712b648d075ffe530543b730373c
Julien Tessier <julien@mac-mini-de-julien.home>
2023-04-07 23:38:09 +0200

    f

--- devis.h
+++ devis.h
@@ -9,10 +9,10 @@
 
 typedef struct	s_devis
 {
-    char	**tabDesignationTaches;
-    int     *nbTachesFactures;    
-    int		*tabPriceTaches;
-    int     nbElements;
+    char	**tabDesignationTaches; //tableau avec les noms des taches
+    int     *nbTachesFactures;      //  tableau avec nombre de fois ou chaque tache a été facturée
+    int		*tabPriceTaches;        // tableau avec le prix de chaque tache
+    int     nbElements;             // nombre de taches différentes
 }				t_devis;
 
 // utils.c
@@ -24,8 +24,5 @@ void    getInformations(t_devis *devis);
 void    recordPriceElements(t_devis *devis);
 void    recordElementsFactures(t_devis *devis);
 void    recordNbElementsFactures(t_devis *devis);
-
-
-
 
 #endif
